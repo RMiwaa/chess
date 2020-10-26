eb3bd6076feec3ebf7829c8c4287a141e9b72998
Roberto Miwa <roberto.miwa@hotmail.com>
2020-10-26 16:41:24 -0300

    Board exceptions and defensive programming

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,25 +2,19 @@ package boardgame;
 
 public class Position {
 
-	private int column;
 	private int row;
+	private int column;
+	
 
 	public Position() {
 			
 		}
 
-	public Position(int column, int row) {
-			this.column = column;
+	public Position( int row, int column) {
 			this.row = row;
+			this.column = column;
+
 		}
-
-	public int getColumn() {
-		return column;
-	}
-
-	public void setColumn(int column) {
-		this.column = column;
-	}
 
 	public int getRow() {
 		return row;
@@ -29,6 +23,16 @@ public class Position {
 	public void setRow(int row) {
 		this.row = row;
 	}
+	
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+
 
 	@Override
 	public String toString() {
