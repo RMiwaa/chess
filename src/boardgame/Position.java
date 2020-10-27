a6c6e2cb164a250f8a5167cfb326c9870796750c
Roberto Miwa <roberto.miwa@hotmail.com>
2020-10-27 18:35:42 -0300

    Rook movement and bug fix

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,11 +4,6 @@ public class Position {
 
 	private int row;
 	private int column;
-	
-
-	public Position() {
-			
-		}
 
 	public Position( int row, int column) {
 			this.row = row;
@@ -32,7 +27,11 @@ public class Position {
 		this.column = column;
 	}
 
-
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
 
 	@Override
 	public String toString() {
